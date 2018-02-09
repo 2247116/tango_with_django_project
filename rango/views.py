3d3646a9d3c277eee55f6e1eab568f67c44828db
Alex Taylor <2247116t@student.gla.ac.uk>
2018-02-09 16:50:49 +0000

    Working

--- rango/views.py
+++ rango/views.py
@@ -103,20 +103,20 @@ def show_category(request, category_name_slug):
 		category = Category.objects.get(slug  = category_name_slug)
 		pages = page.objects.filter(category = category)
 
-# Can we find a category name slug with the given name?
-# If we can't, the .get() method raises a DoesNotExist exception.
-# So the .get() method returns one model instance or raises an exception. category = Category.objects.get(slug=category_name_slug)
-# Retrieve all of the associated pages.
-# Note that filter() will return a list of page objects or an empty list pages = Page.objects.filter(category=category)
-        # Adds our results list to the template context under name pages.
+		# Can we find a category name slug with the given name?
+		# If we can't, the .get() method raises a DoesNotExist exception.
+		# So the .get() method returns one model instance or raises an exception. category = Category.objects.get(slug=category_name_slug)
+		# Retrieve all of the associated pages.
+		# Note that filter() will return a list of page objects or an empty list pages = Page.objects.filter(category=category)
+		        # Adds our results list to the template context under name pages.
 		context_dict['pages'] = pages
-# We also add the category object from
-# the database to the context dictionary.
-# We'll use this in the template to verify that the category exists. context_dict['category'] = category
+		# We also add the category object from
+		# the database to the context dictionary.
+		# We'll use this in the template to verify that the category exists. context_dict['category'] = category
 	except Category.DoesNotExist:
 		cotext_dict['category'] = None
 		context_dict['pages'] = None
-# We get here if we didn't find the specified category. # Don't do anything -
+		# We get here if we didn't find the specified category. # Don't do anything -
 	return render(request, 'rango/category.html', context_dict)
 
 def about(request):
