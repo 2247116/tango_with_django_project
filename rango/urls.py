97367f4611b65c25cffc36f42c6711da20b79d7a
Alex Taylor <2247116t@student.gla.ac.uk>
2018-02-09 17:55:10 +0000

    chap 10

--- rango/urls.py
+++ rango/urls.py
@@ -17,4 +17,9 @@ urlpatterns =[
      url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
         views.add_page,
         name='add_page'),
+
+     url(r'^login/$', views.user_login, name='login'),
+     url(r'^restricted/', views.restricted, name='restricted'),
+     url(r'^logout/$', views.user_logout, name='logout'),
+
 ]
--- rango/views.py
+++ rango/views.py
@@ -12,12 +12,50 @@ from rango.forms import PageForm
 
 from rango.forms import UserForm, UserProfileForm
 
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from datetime import datetime
+
 
 def index(request):
-	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {'categories': category_list}
-    # Render the response and send it back!
-	return render(request, 'rango/index.html', context_dict)
+	request.session.set_test_cookie()
+	category_list = Category.objects.order_by('-likes')[:5] 
+	page_list = page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages': page_list}
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+	response = render(request, 'rango/index.html', context=context_dict) 
+	return response
+
+
+def get_server_side_cookie(request, cookie, default_val = None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+ 	last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+										'%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		#update the last visit cookie now that we have updated the count 
+		request.session['last_visit'] = str(datetime.now())
+
+	else:
+	# set the last visit cookie 
+		request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+	request.session['visits'] = visits
+
 
 def register(request):
 	# A boolean value for telling the template
@@ -92,6 +130,47 @@ def add_page(request, category_name_slug):
 	context_dict = {'form':form, 'category': category}
 	return render(request, 'rango/add_page.html', context_dict)
 
+@login_required
+def user_logout(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('index'))
+
+def restricted(request):
+	return HttpResponse("Since you're logged in, you can see this text!")
+
+
+def user_login(request):
+	# If the request is a HTTP POST, try to pull out the relevant information. 
+	if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        # We use request.POST.get('<variable>') as opposed
+        # to request.POST['<variable>'], because the
+	# request.POST.get('<variable>') returns None if the
+	# value does not exist, while request.POST['<variable>'] # will raise a KeyError exception.
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+	# Use Django's machinery to attempt to see if the username/password # combination is valid - a User object is returned if it is.
+		user = authenticate(username=username, password=password)
+	# If we have a User object, the details are correct.
+	# If None (Python's way of representing the absence of a value), no user # with matching credentials was found.
+		if user:
+	            # Is the account active? It could have been disabled.
+			if user.is_active:
+				# If the account is valid and active, we can log the user in. # We'll send the user back to the homepage.
+				login(request, user)
+				return HttpResponseRedirect(reverse('index'))
+			else:
+					# An inactive account was used - no logging in! 
+				return HttpResponse("Your Rango account is disabled.")
+		else:
+	# Bad login details were provided. So we can't log the user in. 
+			print("Invalid login details: {0}, {1}".format(username, password)) 
+			return HttpResponse("Invalid login details supplied.")
+	# The request is not a HTTP POST, so display the login form. # This scenario would most likely be a HTTP GET.
+	else:
+	# No context variables to pass to the template system, hence the # blank dictionary object...
+		return render(request, 'rango/login.html', {})
 
 def show_category(request, category_name_slug):
 	context_dict = {}
@@ -117,9 +196,13 @@ def show_category(request, category_name_slug):
 		cotext_dict['category'] = None
 		context_dict['pages'] = None
 		# We get here if we didn't find the specified category. # Don't do anything -
+	
 	return render(request, 'rango/category.html', context_dict)
 
 def about(request):
+	if request.session.test_cookie_worked(): 
+		print("TEST COOKIE WORKED!") 
+		request.session.delete_test_cookie()
 
 	print(request.method )
 	print(request.user)
